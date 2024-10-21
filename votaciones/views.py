e7dc71205b34a155b5d35993f8fa77dd88bea0f0
Juan Pablo Hernandez <jpnator18@gmail.com>
2024-10-21 15:14:45 -0600

    Before project

--- votaciones/views.py
+++ votaciones/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
-from .models import Question
+from .models import Question, Choice
+from django.urls import reverse
+from django.db.models import F
+from django.views import generic
+from django.utils import timezone
+
+
+class IndexView(generic.ListView):
+   template_name = "votaciones/index.html"
+   context_object_name = "latest_question_list"
+
+   def get_queryset(self):
+      return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+      # return []
+      # return Question.objects.order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+   model = Question
+   template_name = "votaciones/detail.html"
+   def get_queryset(self):
+       return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+   model = Question
+   template_name = "votaciones/results.html"
+   
 
 
 # Create your views here.
@@ -31,10 +57,24 @@ def detail(request, question_id):
 
 
 def results(request,question_id):
-  response = "You're looking at the results of question %s."
-  return HttpResponse(response % question_id)
+      question = get_object_or_404(Question, pk=question_id)
+      return render(request, "votaciones/results.html", {"question": question})
+  # response = "You're looking at the results of question %s."
+  # return HttpResponse(response % question_id)
 
 def vote(request,question_id):
-  return HttpResponse("You're voting on question %s." % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST["choice"])
+  except (KeyError, Choice.DoesNotExist):
+    return render (request, "votaciones/detail.html", {
+      "question": question,
+      "error_message": "Your didnt select a choice"
+    })
+  else:
+    selected_choice.votes = F("votes") + 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse("votaciones:results", args=(question.id,)))
+  # return HttpResponse("You're voting on question %s." % question_id)
 
 
